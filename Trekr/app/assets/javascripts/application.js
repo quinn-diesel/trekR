b73b88d49394a5aacd7e2574cdc8ff23c3ba3e56
maxbausher <maxbausher@gmail.com>
2017-07-10 10:51:18 +1000

    added leaflet libraries folders into and required leaflet js in app.js file

--- Trekr/app/assets/javascripts/application.js
+++ Trekr/app/assets/javascripts/application.js
@@ -12,4 +12,5 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require leaflet-src
 //= require_tree .
---
+++ Trekr/app/assets/javascripts/hikes.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+
+    console.log("loaded.");
+
+
+
+
+}); // doc ready
