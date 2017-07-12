5f33e84314824e111363c5acb49fe6d0c4fdc93c
Quinn Stobbs <q.stobbs@gmail.com>
2017-07-12 11:51:44 +1000

    established basic site and user functionality

--- Trekr/app/controllers/users_controller.rb
+++ Trekr/app/controllers/users_controller.rb
@@ -14,9 +14,32 @@ before_action :check_if_logged_in, only: [ :hike_create, :update_hikes ]
 
   def create
       @user = User.new(user_params)
+
+        if params[:file].present?
+          # perform upload to cloundinary
+          @user.photo = req['public_id']
+        end
+
+        if @user.save
+          session[:user_id] = @user.id  # perform login (set session)
+          redirect_to user_path(@user.id)   # /users/17
+        else
+          render :new
+        end
+
   end
 
   def update
+    @user = User.find params["id"]
+
+
+    if params[:file].present?
+      @user.photo = req['public_id']
+    end
+
+
+    @user.update user_params
+    redirect_to user_path( params["id"] )
   end
 
   def index
@@ -29,6 +52,7 @@ before_action :check_if_logged_in, only: [ :hike_create, :update_hikes ]
   end
 
   def new
+    @user = User.new
   end
 
   def destroy
